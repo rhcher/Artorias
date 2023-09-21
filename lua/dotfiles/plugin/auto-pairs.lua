5ecbe6b4ea463c20e68085184fb0c458a43fae5b
rhcher <kaer41@qq.com>
2023-09-22 00:15:24 +0800

    misc

--- lua/dotfiles/plugin/auto-pairs.lua
+++ lua/dotfiles/plugin/auto-pairs.lua
@@ -3,4 +3,4 @@ local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local util = autoload("dotfiles.util")
 local pair = require("ultimate-autopair")
-return pair.setup({cr = {conf = {nft = util["lisp-language"]}}, bs = {map = {"<bs>", "<C-w>"}}})
+return pair.setup({cr = {conf = {nft = util["lisp-language"]}}, bs = {map = {"<bs>", "<C-w>"}}, tabout = {enable = true, map = "<tab>", hopout = true, do_nothing_if_fail = false}})
